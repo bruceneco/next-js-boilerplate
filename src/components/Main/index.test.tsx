c3aed3dd3f41f4d4231b59e1ad9e8e403c853e81
Bruce Neco <bruce.neco3@outlook.com>
2021-07-25 18:37:28 -0300

    Add boilerplate image and a sample test for styles

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -5,7 +5,11 @@ describe("<Main/>", () => {
   it("render heading", () => {
     render(<Main />);
     expect(
-      screen.queryByRole("heading", { name: /react project/i })
+      screen.queryByRole("heading", { name: /NextJS Boilerplate/i })
     ).toBeInTheDocument();
+  });
+  it("right background color", () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({ "background-color": "#06092b" });
   });
 });
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from "./index.styles";
+
 const Main = (): JSX.Element => (
-  <main>
-    <h2>React Project</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="img/logo.svg"
+      alt="Atom illustration representing react framework"
+    />
+    <S.Title>NextJS Boilerplate</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled-Components
+    </S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="Programmer in front of code blocks"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
