bc3b61185ed43b2d991ab7e3d02889abc6f33a62
Bruce Neco <bruce.neco3@outlook.com>
2021-07-26 13:45:57 -0300

    Remove knobs use, storybook already implements controls for component arguments

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,19 @@
 import Main from ".";
-import { text, withKnobs } from "@storybook/addon-knobs";
 import { Meta, Story } from "@storybook/react";
 
 export default {
   title: "Main",
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: "Test default",
+    description: "Default also"
+  }
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text("Title", "NextJS Boilerplate")}
-    description={text(
-      "Description",
-      "TypeScript, ReactJS, NextJS and Styled-Components"
-    )}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+
+export const Default: Story = (args) => <Main {...args} />;
+Default.args = {
+  title: "NextJS Boilerplate",
+  description: "TypeScript, NextJS, ReactJS and Styled-Components"
+};
